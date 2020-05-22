84a5b8fe319fe928db584e72e69dc3533e930e27
kerednoslo <olson.derek.r@gmail.com>
2020-05-21 22:23:38 -0700

    shape functions, singular and regular

--- pybem2d.py
+++ pybem2d.py
@@ -15,17 +15,16 @@ def scale_quadrule(eta,wts,a1,b1,a2,b2,etaNew,wtsNew):
   
   def tensorquad(x,wx,y,wy):
        """Combine two quadrules in the x and y direction into a 2D tensor rule"""
-    
+       
        nx=len(x)
        ny=len(y)
        m=numpy.mgrid[0:nx,0:ny].reshape(2,-1)
        x2=numpy.vstack((x[m[0]],y[m[1]]))
        w2=wx[m[0]]*wy[m[1]]
-       return x2,w2   
+       return x2,w2
 class pybem2d:
 
-    
-    
+  
   def __init__(self,nodes,elems,nQuad,nQuadSing,basisOrder,basisType,k):
       """ Construct an instance of the pybem2d class with the geometric and integration information.
       
@@ -49,25 +48,24 @@ class pybem2d:
       self.nQuadSing = nQuadSing
       self.wavenumber = k
       # build quadrature rule
-      self.create_quad_rule()
-      
+ 
       if basisOrder > 1.0:
          print("order not implemented. Error")
          break # this may not work...
       else
           self.basisOrder = order
-          
+      
       
       if basisType == 'DP':
         self.basisType = 'discontinuous'
       elif basisType == 'P':
         self.basisType = 'continuous'
-        
-        
+      
+      
       # compute normals, global_map, eta, functions
       self.prepare_surface()
-        
-        
+      self.define_shape_functions()
+  
   def get_coef_slp(I,J):
       """ Compute coefficient
       
@@ -75,10 +73,19 @@ class pybem2d:
       -----------
       I, - row index of the matrix
       J, - column index of the matrix
-  
+      
       This function is what you need for computing the integral of the Green function
       """
-    
+      # get local dofs
+      
+      # determin whether integration is singular
+      
+      # compute coordinates in real space
+      
+      # add up components from each basis function attached to global dof
+      
+      # return complex 128
+  
   def get_coef_slp_matrix(II,JJ):
     """ Compute coefficient matrix
     
@@ -86,18 +93,18 @@ class pybem2d:
     -----------
     II, - row indicies of the matrix, not necesarily consecutive
     J, - column indices of the matrix, not necessarily consecutive
-
+    
     This function is what you need for computing the integral of the Green function
     block matrix version is used by Pierre Marchand's Htool.
     """
-
+  
   def create_quad_rule(self):
       """computes gauss-quadrature rules and stores result in the class parameters
       Also compute refined quadrature rules for singular elements"""
       eta, wts = _np.polynomial.legendre.leggauss(self.nQuad)
       self.eta = (eta+1)/2.0
       self.wts = wts/2.0;
-      
+  
   def create_refined_composite_quad_rule(self):
       """Adapted from Timo Betcke's pybem2d library"""
       self.x_adapted=numpy.array([])
@@ -110,8 +117,8 @@ class pybem2d:
           self.wts_adapted1D=numpy.hstack([self.wts_adapted1D,(b-a)*self.wts])
           a*=self.sigma
           b*=self.sigma
-
-
+  
+  
   def singular_quad_rule_dp0(self):
       """creates a singular quadrature rule for an element.
       Need to split the integration interval into (0,0.5) and (0.5,1)
@@ -120,32 +127,38 @@ class pybem2d:
       scale_quadrule(self.eta_adapted1D,self.wts_adapted1D,0,1,0.5,1.0,eta_second_half,wts_second_half)
       self.eta_singular_dp0 = _np.hstack([eta_first_half,eta_second_half])
       self.wts_singular_dp0 = _np.hstack([wts_first_half,wts_second_half])
-      
-      
  
           
+  
+  
   def singular_quad_rule_p1(self):
-      """creates a singular quadrature rule for an element.
+      """creates a singular quadrature rule for pair of elements.
      don't need to split the integration up, since the singular integration
-     takes place over two elements"""
-      # using refined quad rule for each element. need to reverse quad rule for first element (idx0) and unreversed for second element (idx1). May be able to generalize to higher-order elements. 
+     takes place over two elements. Need to reverse the integration for the first element."""
+      # using refined quad rule for each element. need to reverse quad rule for first element (idx0) and unreversed for second element (idx1). May be able to generalize to higher-order elements.
       self.eta_singular_p1 = _np.vstack((_np.fliplr(self.eta_adapted1D),self.eta_adapted1D))
       self.wts_singular_p1 = _np.vstack((_np.fliplr(self.wts_adapted1D),self.wts_adapted1D))
-
-       
+  
+  
   def define_shape_functions(self):
       """Define regular shape functions"""
       if self.basisType == 'discontinuous':
-          self.basisNum = 1;
-          self.phin = _np.ones((self.nBasis,1) , dtype=float64);
+          # regular basis functions
+          self.basisNum = 1
+          self.phin = _np.ones(self.eta, dtype=float64)
+          self.phis = _np.ones(self.eta_singular, dtype=float64)
       elif self.basisType == 'continuous':
-          self.basisNum = 1;
-          self.phin = _np.zeros((2 , self.nBasis) , dtype=float64);
+          self.nBasis = 2;
+          self.phin = _np.zeros((2 , self.eta.size) , dtype=float64);
           #regullar shape functions
-          self.phin(1,:) = (1.0 - self.eta)/2
-          self.phin(2,:) = (1.0 + self.eta)/2
+          # these must go to zero when eta is min, and 1 when eta is max, and vice versa.
+          # tthey should sum to one at all points
+          self.phin(1,:) = self.eta;
+          self.phin(2,:) = 1.0 - self.eta;
           # singuar shape functions times the jacobian
-
+          self.phis = _np.zeros((2,self.eta_singular.size), dtype=float64)
+          self.phis(1,:) = self.eta_singular;
+          self.phis(2,:) = 1.0 - self.eta_singular;
   
   def compute_normals(self):
     """ Calculate the normal vector for the mesh """
@@ -164,7 +177,7 @@ class pybem2d:
       normals(1,idx) = dx;
     
     self.normals = normals
-
+  
   def create_global_to_local_map():
     """Create map between global dof numbering and local dofs.
     This will allow a function to requrest the global coefficient number
@@ -177,8 +190,8 @@ class pybem2d:
     gobal_to_local_map: nBasis x nGlobalDOF
     The values in this array correspond to the element number over which the
     the integration occurs.
-      
-    The first index corresponds the basis functions. For DP0, there is only one 
+    
+    The first index corresponds the basis functions. For DP0, there is only one
     basis function. For P1, there are two basis function. For now, no more basis
     functions are planned to be implemented
     
@@ -195,13 +208,13 @@ class pybem2d:
       # loop through the elements in order, which are the same as global DOF.
       for idx in range(0,self.nElems):
           global_to_local_map[0,idx] = idx;
-      self.global_to_local_map = global_to_local_map 
+      self.global_to_local_map = global_to_local_map
       # disable checking boundary nodes and elems for now...
       self.boundaryNodes = None
       self.boundaryElems = None
-      
+    
     elif self.basisType == 'continuous':
-        # linear basis functions have two dofs that contribute to each 
+        # linear basis functions have two dofs that contribute to each
         global_to_local_map = np.full((2 , self.nNodes) , -1,dtype=int)
         # loop through nodes, which are the same as global DOF
         # Nodes to elems map is a list of elements that are attached to each node
@@ -220,29 +233,45 @@ class pybem2d:
         # end node loop.
         self.global_to_local_map = global_to_local_map
         
-        # loop through to check if there are any boundary points. 
+        # loop through to check if there are any boundary points.
         # Boundary points are nodes that are attached to only one element
         # Actuall, this information is already in there. if global_to_local_map ==-1, then we are on a boundary.
-        # if global DOF is a boundary node, then return zero for the coefficient. 
-
-    
+        # if global DOF is a boundary node, then return zero for the coefficient.
    
             
         
     
+  
+  
   def prepare_surface(self):
     nodesShape = self.nodes.shape()
     if nodesShape[0] != 2
       print("Nodes must have a shape of 2xNN, where NN is the number of nodes")
-   self.nNodes = nodesShape[1]
+    self.nNodes = nodesShape[1]
    
-   elemsShape = self.elems.shape()
-   if elemsShape[0] != 2
+    elemsShape = self.elems.shape()
+    if elemsShape[0] != 2
       print("Elems must have a shape of 2xNE, where NE is the number of elems")
-   self.nElems = elemsShape[1]
+    self.nElems = elemsShape[1]
    
-   self.nNodes = nNodes
-   self.nElems = nElems
-   self.compute_normals()
-   self.create_global_to_local_map()
+    self.nNodes = nNodes
+    self.nElems = nElems
+    self.compute_normals()
+    self.create_global_to_local_map()
+
   
+  def prepare_quad_rules(self):
+      """choose appropriate quadrature rule and stores in
+      self.eta_singular and self.wts_singular. Each of these arrays
+      has size nBasisFunctions x nQuad"""
+    self.create_quad_rule()
+    self.create_refined_composite_quad_rule()
+    if self.basisType == 'discontinuous':
+        self.singular_quad_rule_dp0()
+        self.eta_singular = self.eta_singular_dp0;
+        self.wts_singular = self.wts_singular_dp0;
+    elif  self.basisType == 'continuous':
+        self.singular_quad_rule_p1()
+        self.eta_singular = self.eta_singular_p1;
+        self.wts_singular = self.wts_singular_p1;
+        
